428e1254b4a2487398d4184927576b77a781648b
liyanarosliescs <liyana.roslie@scs71.com>
2019-09-02 12:43:56 +0800

    change image sign in

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -12,6 +12,7 @@ import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import signInImg from '../images/signInImg.jpg';
 
 function Copyright() {
   return (
@@ -30,7 +31,8 @@ const useStyles = makeStyles(theme => ({
     height: '100vh',
   },
   image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundImage: 'url(' + signInImg + ')',
+    alt: 'Sign In',
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
     backgroundPosition: 'center',
