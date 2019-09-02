fbd06b0c90c58082dbcef92503a79eb934e07f23
liyanarosliescs <liyana.roslie@scs71.com>
2019-09-02 19:03:49 +0800

    add sign up 2 page

---
+++ src/components/SignUp2.js
@@ -0,0 +1,180 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import signInImg from '../images/signInImg.jpg';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Ostrich
+      </Link>{' '}
+      {new Date().getFullYear()}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(' + signInImg + ')',
+    alt: 'Sign In',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="login-email"
+              label="Login Email Address"
+              name="login-email"
+              autoComplete="login-email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="notification-email"
+              label="Notification Email Address"
+              name="notification-email"
+              autoComplete="notification-email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="confirm-password"
+              label="Confirm Password"
+              type="password"
+              id="confirm-password"
+              autoComplete="confirm-password"
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="company-name"
+              label="Company Name"
+              name="company-name"
+              autoComplete="company-name"
+              autoFocus
+            />
+            <RadioGroup aria-label="position" name="position" row>
+            <Grid item xs={6}>
+              <FormControlLabel
+                value="shipper"
+                control={<Radio color="primary" />}
+                label="Shipper"
+                labelPlacement="shipper"
+              />
+              </Grid>
+              <Grid item xs={6}>
+              <FormControlLabel
+                value="carrier"
+                control={<Radio color="primary" />}
+                label="Carrier"
+                labelPlacement="Carrier"
+              />
+              </Grid>
+            </RadioGroup>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign Up
+            </Button>
+            <Grid container justify="center">
+              By signing in, you agree to our&nbsp;
+              <Link href="#" variant="body2">
+                Terms of Service
+              </Link>
+            </Grid>
+            <Grid container justify="center">
+              <Grid item>
+                Already have an account?&nbsp;
+                <Link href="#" variant="body2">
+                  Sign in
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import App from './components/App';
 import SignUp from './components/SignUp'
+import SignUp2 from './components/SignUp2'
 
 const routing = (
   <Router>
     <div>
       <Route exact path="/" component={App} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/signup2" component={SignUp2} />
     </div>
   </Router>
 )
