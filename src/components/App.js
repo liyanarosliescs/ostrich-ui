17c6b5ff8e565a55c84313d9fa0eaf9e326ba85a
liyanarosliescs <liyana.roslie@scs71.com>
2019-09-02 14:10:58 +0800

    refactor App.js and add Sign Up page

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import NavBar from './components/NavBar'
-import SignIn from './components/SignIn'
+import SignIn from './SignIn'
 class App extends Component {
   render() {
     return (
