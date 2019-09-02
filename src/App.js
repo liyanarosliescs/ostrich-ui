99ccf3a3c790760a44542e3523b50b9e459e33a2
liyanarosliescs <liyana.roslie@scs71.com>
2019-09-02 11:21:51 +0800

    add sign in page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import NavBar from './components/NavBar'
+import SignIn from './components/SignIn'
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
+        <SignIn />
       </div>
     )
   }
