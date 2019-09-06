f531f59c005998a527dea74b564c2540d0b36617
liyanarosliescs <liyana.roslie@scs71.com>
2019-09-06 14:27:31 +0800

    add shipper work order page

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import App from './components/App';
 import SignUp from './components/SignUp'
+import ShipperWorkOrder from './components/ShipperWorkOrder'
 
 const routing = (
   <Router>
     <div>
       <Route exact path="/" component={App} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/shipper/workorder" component={ShipperWorkOrder} />
     </div>
   </Router>
 )
