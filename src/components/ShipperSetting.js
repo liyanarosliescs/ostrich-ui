f45d557e8cd9a4bd7bafe295a39c5ce2bb19ba17
liyanarosliescs <liyana.roslie@scs71.com>
2019-09-18 16:36:58 +0800

    add password fields in password tab

--- src/components/ShipperSetting.js
+++ src/components/ShipperSetting.js
@@ -374,7 +374,47 @@ function ShipperSetting(props) {
             </Grid>
           </TabPanel>
           <TabPanel value={value} index={1}>
-            Item Two
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              alignItems="center">
+              <form className={classes.form} noValidate>
+                <TextField
+                  margin="normal"
+                  fullWidth
+                  id="current-password"
+                  label="Current Password"
+                  name="current-password"
+                  autoComplete="current-password"
+                  type="password"
+                  autoFocus/>
+                <TextField
+                  margin="normal"
+                  fullWidth
+                  id="new-password"
+                  label="New Password"
+                  name="new-password"
+                  autoComplete="new-password"
+                  type="password"/>
+                <TextField
+                  margin="normal"
+                  fullWidth
+                  id="confirm-new-password"
+                  label="Confirm New Password"
+                  name="confirm-new-password"
+                  autoComplete="confirm-new-password"
+                  type="password"/>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}>
+                  Update
+                </Button>
+              </form>
+            </Grid>
           </TabPanel>
           <TabPanel value={value} index={2}>
             Item Three
