6bdca9b37e1b909ef4bbbff0b7a5a6788d02872b
liyanarosliescs <liyana.roslie@scs71.com>
2019-09-19 17:49:14 +0800

    add more currency mappings

--- src/components/Currencies.js
+++ src/components/Currencies.js
@@ -12,12 +12,136 @@ const currencies = [
     label: 'MXN $',
   },
   {
+    value: 'THB',
+    label: 'THB ฿',
+  },
+  {
     value: 'JPY',
     label: 'JPY ¥',
   },
   {
     value: 'EUR',
     label: 'EUR €',
+  },
+  {
+    value: 'AFN',
+    label: 'AFN ؋',
+  },
+  {
+    value: 'ALL',
+    label: 'ALL L',
+  },
+  {
+    value: 'DZD',
+    label: 'DZD دج',
+  },
+  {
+    value: 'AOA',
+    label: 'AOA Kz',
+  },
+  {
+    value: 'XCD',
+    label: 'XCD ‎$',
+  },
+  {
+    value: 'ARS',
+    label: 'ARS ‎$',
+  },
+  {
+    value: 'AMD',
+    label: 'AMD ֏',
+  },
+  {
+    value: 'AWG',
+    label: 'AWG ƒ',
+  },
+  {
+    value: 'AUD',
+    label: 'AUD ‎$',
+  },
+  {
+    value: 'AZN',
+    label: 'AZN ₼',
+  },
+  {
+    value: 'BSD',
+    label: 'BSD $',
+  },
+  {
+    value: 'BHD',
+    label: 'BHD .د.ب',
+  },
+  {
+    value: 'BDT',
+    label: 'BDT ৳',
+  },
+  {
+    value: 'BBD',
+    label: 'BBD $',
+  },
+  {
+    value: 'BYN',
+    label: 'BYN Br',
+  },
+  {
+    value: 'BZD',
+    label: 'BZD $',
+  },
+  {
+    value: 'XOF',
+    label: 'XOF CFA',
+  },
+  {
+    value: 'BMD',
+    label: 'BMD $',
+  },
+  {
+    value: 'BTN',
+    label: 'BTN Nu.',
+  },
+  {
+    value: 'INR',
+    label: 'INR ₹',
+  },
+  {
+    value: 'BOB',
+    label: 'BOB Bs',
+  },
+  {
+    value: 'BOV',
+    label: 'BOV',
+  },
+  {
+    value: 'BAM',
+    label: 'BAM KM',
+  },
+  {
+    value: 'BWP',
+    label: 'BWP P',
+  },
+  {
+    value: 'NOK',
+    label: 'NOK kr',
+  },
+  {
+    value: 'BRL',
+    label: 'BRL R$',
+  },
+  {
+    value: 'BND',
+    label: 'BND B$',
+  },
+  {
+    value: 'BGN',
+    label: 'BGN Лв.',
+  },
+  {
+    value: 'BIF',
+    label: 'BIF FBu',
+  },
+  {
+    value: 'CVE',
+    label: 'CVE $',
   },
 ];
 
