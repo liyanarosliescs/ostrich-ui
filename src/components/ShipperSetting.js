f8b5327c9efc386095532a5c43e0e556f0518254
liyanarosliescs <liyana.roslie@scs71.com>
2019-09-20 11:54:11 +0800

    separate out the sideDrawer component

--- src/components/ShipperSetting.js
+++ src/components/ShipperSetting.js
@@ -2,30 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Badge from '@material-ui/core/Badge';
 import NotificationsIcon from '@material-ui/icons/Notifications';
-import Avatar from '@material-ui/core/Avatar';
-import Grid from '@material-ui/core/Grid';
-import shipperIcon from '../images/shipperIcon.png';
 import Link from '@material-ui/core/Link';
 import ExitIcon from '@material-ui/icons/ExitToApp';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import WorkOrderIcon from '@material-ui/icons/Description';
-import OrderFulfillmentIcon from '@material-ui/icons/Assignment';
-import ShipmentIcon from '@material-ui/icons/LocalShipping';
-import ContactsIcon from '@material-ui/icons/Contacts';
 import SettingsIcon from '@material-ui/icons/Build';
 import Paper from '@material-ui/core/Paper';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
@@ -40,6 +27,7 @@ import AccountTab from './AccountTab';
 import PasswordTab from './PasswordTab';
 import EmailTab from './EmailTab';
 import CurrencyTab from './CurrencyTab';
+import SideDrawer from './SideDrawer';
 
 const drawerWidth = 240;
 
@@ -77,9 +65,6 @@ const useStyles = makeStyles(theme => ({
     },
   },
   toolbar: theme.mixins.toolbar,
-  profile: {
-    marginTop: theme.spacing(2),
-  },
   drawerPaper: {
     width: drawerWidth,
   },
@@ -89,18 +74,6 @@ const useStyles = makeStyles(theme => ({
   },
   toolbarButtons: {
     marginLeft: 'auto',
-  },
-  avatar: {
-    margin: 10,
-    width: 70,
-    height: 70,
-  },
-  name: {
-    paddingTop: theme.spacing(1),
-  },
-  link: {
-    paddingTop: theme.spacing(1),
-    paddingBottom: theme.spacing(2),
   },
   paper: {
     marginLeft: theme.spacing(25),
@@ -177,57 +150,6 @@ function ShipperSetting(props) {
     setValue(newValue);
   }
 
-  const drawer = (
-    <div>
-      <div className={classes.profile} />
-      <Grid container justify="center" alignItems="center">
-        <Avatar alt="Shipper" src={shipperIcon} className={classes.avatar} />
-      </Grid>
-      <Grid container justify="center" alignItems="center">
-        <Typography variant="h5" className={classes.name}>
-          Company Name
-        </Typography>
-      </Grid>
-      <Grid container justify="center" alignItems="center">
-        <Typography variant="body1" className={classes.name}>
-          User Name
-        </Typography>
-      </Grid>
-      <Grid container justify="center" alignItems="center">
-        <Link href="#" variant="body2" className={classes.link}>
-          Edit Profile
-        </Link>
-      </Grid>
-      <Divider />
-      <List>
-        <ListItem button key="dashboardBtn">
-          <ListItemIcon><DashboardIcon /></ListItemIcon>
-          <ListItemText primary="Dashboard" />
-        </ListItem>
-        <ListItem button key="workOrderBtn">
-          <ListItemIcon><WorkOrderIcon /></ListItemIcon>
-          <ListItemText primary="Work Order" />
-        </ListItem>
-        <ListItem button key="orderFulfillmentBtn">
-          <ListItemIcon><OrderFulfillmentIcon /></ListItemIcon>
-          <ListItemText primary="Order Fulfillment" />
-        </ListItem>
-        <ListItem button key="shipmentTypeBtn">
-          <ListItemIcon><ShipmentIcon /></ListItemIcon>
-          <ListItemText primary="Shipment Type" />
-        </ListItem>
-        <ListItem button key="contactsDirectoryBtn">
-          <ListItemIcon><ContactsIcon /></ListItemIcon>
-          <ListItemText primary="Contacts Directory" />
-        </ListItem>
-        <ListItem button key="settingsBtn">
-          <ListItemIcon><SettingsIcon /></ListItemIcon>
-          <ListItemText primary="Settings" />
-        </ListItem>
-      </List>
-    </div>
-  );
-
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -272,7 +194,7 @@ function ShipperSetting(props) {
               keepMounted: true, // Better open performance on mobile.
             }}
           >
-            {drawer}
+            <SideDrawer/>
           </Drawer>
         </Hidden>
         <Hidden xsDown implementation="css">
@@ -283,7 +205,7 @@ function ShipperSetting(props) {
             variant="permanent"
             open
           >
-            {drawer}
+            <SideDrawer/>
           </Drawer>
         </Hidden>
       </nav>
---
+++ src/components/SideDrawer.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import WorkOrderIcon from '@material-ui/icons/Description';
+import OrderFulfillmentIcon from '@material-ui/icons/Assignment';
+import ShipmentIcon from '@material-ui/icons/LocalShipping';
+import ContactsIcon from '@material-ui/icons/Contacts';
+import SettingsIcon from '@material-ui/icons/Build';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import shipperIcon from '../images/shipperIcon.png';
+
+const useStyles = makeStyles(theme => ({
+  profile: {
+    marginTop: theme.spacing(2),
+  },
+  avatar: {
+    margin: 10,
+    width: 70,
+    height: 70,
+  },
+  name: {
+    paddingTop: theme.spacing(1),
+  },
+  link: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(2),
+  },
+}));
+
+function SideDrawer(props) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <div className={classes.profile} />
+      <Grid container justify="center" alignItems="center">
+        <Avatar alt="Shipper" src={shipperIcon} className={classes.avatar} />
+      </Grid>
+      <Grid container justify="center" alignItems="center">
+        <Typography variant="h5" className={classes.name}>
+          Company Name
+        </Typography>
+      </Grid>
+      <Grid container justify="center" alignItems="center">
+        <Typography variant="body1" className={classes.name}>
+          User Name
+        </Typography>
+      </Grid>
+      <Grid container justify="center" alignItems="center">
+        <Link href="#" variant="body2" className={classes.link}>
+          Edit Profile
+        </Link>
+      </Grid>
+      <Divider />
+      <List>
+        <ListItem button key="dashboardBtn">
+          <ListItemIcon><DashboardIcon /></ListItemIcon>
+          <ListItemText primary="Dashboard" />
+        </ListItem>
+        <ListItem button key="workOrderBtn">
+          <ListItemIcon><WorkOrderIcon /></ListItemIcon>
+          <ListItemText primary="Work Order" />
+        </ListItem>
+        <ListItem button key="orderFulfillmentBtn">
+          <ListItemIcon><OrderFulfillmentIcon /></ListItemIcon>
+          <ListItemText primary="Order Fulfillment" />
+        </ListItem>
+        <ListItem button key="shipmentTypeBtn">
+          <ListItemIcon><ShipmentIcon /></ListItemIcon>
+          <ListItemText primary="Shipment Type" />
+        </ListItem>
+        <ListItem button key="contactsDirectoryBtn">
+          <ListItemIcon><ContactsIcon /></ListItemIcon>
+          <ListItemText primary="Contacts Directory" />
+        </ListItem>
+        <ListItem button key="settingsBtn">
+          <ListItemIcon><SettingsIcon /></ListItemIcon>
+          <ListItemText primary="Settings" />
+        </ListItem>
+      </List>
+    </div>
+  );
+}
+
+SideDrawer.propTypes = {
+  container: PropTypes.instanceOf(typeof Element === 'undefined' ? Object : Element),
+};
+
+export default SideDrawer;
