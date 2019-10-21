1a1e3b1eac3947bda9ee1cb0022589f5e90e74a5
liyanarosliescs <liyana.roslie@scs71.com>
2019-10-21 17:32:39 +0800

    add datepicker, location and empty/loaded radio input in JourneyForm

--- src/components/Shipper/CreateWorkOrderComponent/JourneyForm.js
+++ src/components/Shipper/CreateWorkOrderComponent/JourneyForm.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
+import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
+import JourneyIconHorizontal from '@material-ui/icons/ArrowForward';
+import JourneyIconVertical from '@material-ui/icons/ArrowDownward';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
@@ -23,11 +28,29 @@ const useStyles = makeStyles(theme => ({
   textField: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
-  }
+  },
+  dateField: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
+  icon: {
+    marginTop: theme.spacing(20),
+    marginLeft: theme.spacing(2),
+    [theme.breakpoints.down('sm')]: {
+      marginTop: theme.spacing(2),
+      marginLeft: theme.spacing(20),
+    },
+    width: 70,
+    height: 70,
+  },
 }));
 
 export default function JourneyForm() {
   const classes = useStyles();
+
+  const [startDate, setStartDate] = useState(null);
 
   return (
     <React.Fragment>
@@ -37,7 +60,7 @@ export default function JourneyForm() {
       <Grid container spacing={3}>
         <Grid item xs={12}>
         </Grid>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={5}>
           <Card className={classes.card}>
             <CardHeader
               title="Pick Up"
@@ -65,6 +88,17 @@ export default function JourneyForm() {
                   className={classes.textField}
                   fullWidth
                 />
+                <DatePicker
+                  placeholderText="* Select Date/Time"
+                  className={classes.dateField}
+                  selected={startDate}
+                  onChange={date => setStartDate(date)}
+                  showTimeSelect
+                  timeFormat="HH:mm"
+                  timeIntervals={10}
+                  timeCaption="time"
+                  dateFormat="dd/MM/yyyy h:mm aa"
+                />
                 <RadioGroup aria-label="position" name="position" row>
                   <FormControlLabel
                     value="empty"
@@ -83,7 +117,19 @@ export default function JourneyForm() {
             </CardContent>
           </Card>
         </Grid>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={2}>
+          <Box display={{ xs: 'none', sm: 'none', md: 'block', lg: 'block' }}>
+            <JourneyIconHorizontal
+              className={classes.icon}
+            />
+          </Box>
+          <Box display={{ xs: 'block', sm: 'block', md: 'none', lg: 'none' }}>
+            <JourneyIconVertical
+              className={classes.icon}
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={5}>
           <Card className={classes.card}>
             <CardHeader
               title="Delivery"
@@ -111,6 +157,17 @@ export default function JourneyForm() {
                   label="Location Line 4"
                   className={classes.textField}
                   fullWidth
+                />
+                <DatePicker
+                  placeholderText="* Select Date/Time"
+                  className={classes.dateField}
+                  selected={startDate}
+                  onChange={date => setStartDate(date)}
+                  showTimeSelect
+                  timeFormat="HH:mm"
+                  timeIntervals={10}
+                  timeCaption="time"
+                  dateFormat="dd/MM/yyyy h:mm aa"
                 />
               </form>
             </CardContent>
