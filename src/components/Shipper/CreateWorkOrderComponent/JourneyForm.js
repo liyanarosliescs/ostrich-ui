43f05350458da537d5cbac4c0b636e8e74ded4fd
liyanarosliescs <liyana.roslie@scs71.com>
2019-10-21 14:02:10 +0800

    add journey form

--- src/components/Shipper/CreateWorkOrderComponent/JourneyForm.js
+++ src/components/Shipper/CreateWorkOrderComponent/JourneyForm.js
@@ -3,38 +3,118 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 400,
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  }
+}));
 
 export default function JourneyForm() {
+  const classes = useStyles();
+
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
         Journey Information
       </Typography>
       <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <TextField required id="cardName" label="Name on card" fullWidth />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField required id="cardNumber" label="Card number" fullWidth />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField required id="expDate" label="Expiry date" fullWidth />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cvv"
-            label="CVV"
-            helperText="Last three digits on signature strip"
-            fullWidth
-          />
-        </Grid>
         <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-            label="Remember credit card details for next time"
-          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Card className={classes.card}>
+            <CardHeader
+              title="Pick Up"
+            />
+            <CardContent>
+              <form className={classes.container}>
+                <TextField
+                  label="Location Line 1"
+                  className={classes.textField}
+                  fullWidth
+                  required
+                />
+                <TextField
+                  label="Location Line 2"
+                  className={classes.textField}
+                  fullWidth
+                />
+                <TextField
+                  label="Location Line 3"
+                  className={classes.textField}
+                  fullWidth
+                />
+                <TextField
+                  label="Location Line 4"
+                  className={classes.textField}
+                  fullWidth
+                />
+                <RadioGroup aria-label="position" name="position" row>
+                  <FormControlLabel
+                    value="empty"
+                    control={<Radio color="primary" />}
+                    label="Empty"
+                    labelPlacement="empty"
+                  />
+                  <FormControlLabel
+                    value="loaded"
+                    control={<Radio color="primary" />}
+                    label="Loaded"
+                    labelPlacement="loaded"
+                  />
+                </RadioGroup>
+              </form>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Card className={classes.card}>
+            <CardHeader
+              title="Delivery"
+              className={classes.cardContent}
+            />
+            <CardContent>
+              <form className={classes.container}>
+                <TextField
+                  label="Location Line 1"
+                  className={classes.textField}
+                  fullWidth
+                  required
+                  />
+                  <TextField
+                  label="Location Line 2"
+                  className={classes.textField}
+                  fullWidth
+                  />
+                  <TextField
+                  label="Location Line 3"
+                  className={classes.textField}
+                  fullWidth
+                  />
+                  <TextField
+                  label="Location Line 4"
+                  className={classes.textField}
+                  fullWidth
+                />
+              </form>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
     </React.Fragment>
