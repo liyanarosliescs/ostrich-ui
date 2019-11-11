ed1c5cb0d3e0a816a725f4a7a096b9e07c0aac51
liyanarosliescs <liyana.roslie@scs71.com>
2019-11-11 19:11:13 +0800

    add sample form 4

---
+++ src/components/Shipper/TestForm4.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+const TestForm4 = () => {
+    const blankCat = { name: '', age: '' };
+    const [catState, setCatState] = useState([
+        { ...blankCat},
+    ]);
+
+    const addCat = () => {
+        setCatState([...catState, { ...blankCat }]);
+    };
+
+    return (
+        <form>
+            <label htmlFor="owner">Owner</label>
+            <input type="text" name="owner" id="owner" />
+            <label htmlFor="description">Description</label>
+            <input
+                type="text"
+                name="description"
+                id="description"
+            />
+            <input
+                type="button"
+                value="Add New Cat"
+                onClick={addCat}
+            />
+            {
+                catState.map((val, idx) => {
+                    const catId = `name-${idx}`;
+                    const ageId = `age-${idx}`;
+                    return (
+                        <div key={`cat-${idx}`}>
+                            <label htmlFor={catId}>{`Cat #${idx + 1}`}</label>
+                            <input
+                                type="text"
+                                name={catId}
+                                data-idx={idx}
+                                id={catId}
+                                className="name"
+                            />
+                            <label htmlFor={ageId}>Age</label>
+                            <input
+                                type="text"
+                                name={ageId}
+                                data-idx={idx}
+                                id={ageId}
+                                className="age"
+                            />
+                        </div>
+                    );
+                })
+            }
+            <input type="submit" value="Submit" />
+        </form>
+    );
+};
+
+export default TestForm4;
--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ import CreateWorkOrder from './components/Shipper/CreateWorkOrder'
 import TestDynamicForm from './components/Shipper/TestDynamicForm'
 import TestForm2 from './components/Shipper/TestForm2'
 import TestForm3 from './components/Shipper/TestForm3'
+import TestForm4 from './components/Shipper/TestForm4'
 
 const routing = (
   <Router>
@@ -49,6 +50,7 @@ const routing = (
       <Route exact path="/shipper/form" component={TestDynamicForm} />
       <Route exact path="/shipper/form2" component={TestForm2} />
       <Route exact path="/shipper/form3" component={TestForm3} />
+      <Route exact path="/shipper/form4" component={TestForm4} />
       <Route exact path="/trucker/setting" component={TruckerSetting} />
       <Route exact path="/trucker/masterdata" component={MasterData} />
       <Route exact path="/trucker/jobs" component={AvailableJobs} />
