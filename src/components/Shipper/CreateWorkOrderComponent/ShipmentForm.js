cd8a4a808b8654c7b9ffb67a00e1b0588b46d655
liyanarosliescs <liyana.roslie@scs71.com>
2019-11-12 17:39:05 +0800

    revert ShipmentForm to original

--- src/components/Shipper/CreateWorkOrderComponent/ShipmentForm.js
+++ src/components/Shipper/CreateWorkOrderComponent/ShipmentForm.js
@@ -20,7 +20,6 @@ import DeleteBox from "@material-ui/icons/IndeterminateCheckBox";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { makeStyles } from '@material-ui/core/styles';
-import ShipmentAddContainer from './ShipmentAddContainer';
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -238,7 +237,30 @@ export default function ShipmentForm() {
           </Box>
         </Grid>
         <Grid item xs={12} md={3}>
-          <ShipmentAddContainer />
+          <Card className={classes.card}>
+            <Typography variant="h6" className={classes.title}>
+              Container <AddBox/> <DeleteBox/>
+            </Typography>
+            <CardContent>
+              <form className={classes.container}>
+                <TextField
+                  label="Vehicle No"
+                  className={classes.textField}
+                  fullWidth
+                  />
+                  <TextField
+                  label="Seal No"
+                  className={classes.textField}
+                  fullWidth
+                  />
+                  <TextField
+                  label="VGM"
+                  className={classes.textField}
+                  fullWidth
+                  />
+              </form>
+            </CardContent>
+          </Card>
         </Grid>
         <Grid item xs={12} md={1}>
           <Box display={{ xs: 'none', sm: 'none', md: 'block', lg: 'block' }}>
