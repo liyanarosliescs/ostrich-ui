e894dbf223fec1b2d5f65fe0ad4ad4ee44f36f9b
liyanarosliescs <liyana.roslie@scs71.com>
2019-11-18 17:24:03 +0800

    modify TestForm14

---
+++ src/components/Common/MySelect.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import Select from 'react-select';
+
+const options = [
+  { value: 'Food', label: 'Food' },
+  { value: 'Being Fabulous', label: 'Being Fabulous' },
+  { value: 'Ken Wheeler', label: 'Ken Wheeler' },
+  { value: 'ReasonML', label: 'ReasonML' },
+  { value: 'Unicorns', label: 'Unicorns' },
+  { value: 'Kittens', label: 'Kittens' },
+];
+
+class MySelect extends React.Component {
+  handleChange = value => {
+    // this is going to call setFieldValue and manually update values.topcis
+    this.props.onChange('topics', value);
+  };
+
+  handleBlur = () => {
+    // this is going to call setFieldTouched and manually update touched.topcis
+    this.props.onBlur('topics', true);
+  };
+
+  render() {
+    return (
+      <div style={{ margin: '1rem 0' }}>
+        <label htmlFor="color">Topics (select at least 3) </label>
+        <Select
+          id="color"
+          options={options}
+          isMulti
+          onChange={this.handleChange}
+          onBlur={this.handleBlur}
+          value={this.props.value}
+        />
+        {!!this.props.error &&
+          this.props.touched && (
+            <div style={{ color: 'red', marginTop: '.5rem' }}>{this.props.error}</div>
+          )}
+      </div>
+    );
+  }
+}
+
+export default MySelect;
--- src/components/Shipper/TestForm14.js
+++ src/components/Shipper/TestForm14.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withFormik } from 'formik';
-import Select from 'react-select';
+import { MoreResources } from '../Common/FormikHelper';
+import MySelect from '../Common/MySelect';
 
 const formikEnhancer = withFormik({
   mapPropsToValues: props => ({
@@ -9,7 +10,7 @@ const formikEnhancer = withFormik({
   })
 });
 
-export const TestForm14 = props => {
+const MyForm = props => {
   const {
     values,
     touched,
@@ -40,6 +41,13 @@ export const TestForm14 = props => {
         touched.email && (
           <div style={{ color: 'red', marginTop: '.5rem' }}>{errors.email}</div>
         )}
+      <MySelect
+        value={values.topics}
+        onChange={setFieldValue}
+        onBlur={setFieldTouched}
+        error={errors.topics}
+        touched={touched.topics}
+      />
       <button
         type="button"
         className="outline"
@@ -51,8 +59,34 @@ export const TestForm14 = props => {
       <button type="submit" disabled={isSubmitting}>
         Submit
       </button>
+
+      <pre>{JSON.stringify(values, null, 2)}</pre>
     </form>
   );
 };
+
+const MyEnhancedForm = formikEnhancer(MyForm);
+
+const TestForm14 = () => (
+  <div className="app">
+    <h1>
+      Using <a href="https://github.com/jaredpalmer/formik">Formik</a> with 3rd-party
+      input components
+    </h1>
+    <p>
+      This example shows to use Formik with a 3rd-party input component. The trick is to
+      use Formik's <code>setFieldValue</code> prop and a custom component class whenever
+      you need a custom change handler.{' '}
+    </p>
+    <p>
+      To show this off, below is a Formik-enhanced form. It has a "vanilla" Formik input
+      for <code>email</code> and a custom select component for <code>topics</code> that
+      uses Jed Watson's{' '}
+      <a href="https://github.com/JedWatson/react-select">react-select</a> library.
+    </p>
+    <MyEnhancedForm/>
+    <MoreResources />
+  </div>
+);
 
 export default TestForm14;
