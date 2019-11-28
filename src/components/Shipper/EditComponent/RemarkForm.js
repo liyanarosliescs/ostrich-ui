df49ed6835410203d7dd1e20f2f3533debc87b2c
liyanarosliescs <liyana.roslie@scs71.com>
2019-11-28 14:46:56 +0800

    refactor remarkform in shipper edit page

--- src/components/Shipper/EditComponent/RemarkForm.js
+++ src/components/Shipper/EditComponent/RemarkForm.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import Button from '@material-ui/core/Button';
 import Table from '@material-ui/core/Table';
