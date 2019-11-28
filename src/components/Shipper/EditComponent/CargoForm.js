cf49c5d252197bedbb9329be87470b421d8f630b
liyanarosliescs <liyana.roslie@scs71.com>
2019-11-28 12:13:38 +0800

    refactor cargoform in shipper edit page

--- src/components/Shipper/EditComponent/CargoForm.js
+++ src/components/Shipper/EditComponent/CargoForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import { TextField } from 'formik-material-ui';
 import IconButton from '@material-ui/core/IconButton';
@@ -13,18 +12,6 @@ import { Formik, Field, Form, FieldArray } from "formik";
 const useStyles = makeStyles(theme => ({
   title: {
     color: "#3f51b5"
-  },
-  icon: {
-    width: 40,
-    height: 40,
-  },
-  card: {
-    padding: theme.spacing(2),
-    margin: theme.spacing(2)
-  },
-  select: {
-    padding: theme.spacing(1),
-    margin: theme.spacing(1)
   },
   textField: {
     marginLeft: theme.spacing(2),
