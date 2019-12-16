948b49bea7a842f9349dc6d946680886e40fda35
liyanarosliescs <liyana.roslie@scs71.com>
2019-12-16 18:51:36 +0800

    cardform - styling

--- src/components/Shipper/CardForm.js
+++ src/components/Shipper/CardForm.js
@@ -1,15 +1,33 @@
 import React, { useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
+import AddBox from "@material-ui/icons/AddBox";
+import DeleteBox from "@material-ui/icons/IndeterminateCheckBox";
 import uuid from 'uuid';
 
 const useStyles = makeStyles(theme => ({
-  card: {
+  paper: {
     padding: theme.spacing(1),
     margin: theme.spacing(2)
+  },
+  card: {
+    padding: theme.spacing(1),
+    marginLeft: theme.spacing(7),
+    marginRight: theme.spacing(7),
+    marginTop:theme.spacing(2),
+    marginBottom: theme.spacing(2)
+  },
+  parentCard: {
+    padding: theme.spacing(1),
+    margin: theme.spacing(2),
+    backgroundColor: "#3f51b5"
+  },
+  parentCardContent: {
+    color: "#ffffff"
   },
   cardButton: {
     display: "block",
@@ -34,24 +52,30 @@ export default function CardForm() {
   return (
     <Grid container justify = "center">
       <Grid item xs={12}>
-        <Card className={classes.card}>
-          <CardContent>Shipment</CardContent>
+        <Paper className={classes.paper}>
+          Shipment
           <Grid container justify = "center">
             <Grid item xs={6}>
-              <Card className={classes.card}>
-                <CardContent>Container</CardContent>
+              <Card className={classes.parentCard}>
+                <CardContent className={classes.parentCardContent}>Container</CardContent>
                 <Card className={classes.card}>
-                  <CardContent>Container A</CardContent>
+                  <CardContent>
+                    <IconButton><AddBox/></IconButton>
+                    <div>
+                      Container A <br/>
+                      <IconButton><DeleteBox/></IconButton>
+                    </div>
+                  </CardContent>
                 </Card>
               </Card>
             </Grid>
             <Grid item xs={6}>
-              <Card className={classes.card}>
-                <CardContent>Cargo</CardContent>
+              <Card className={classes.parentCard}>
+                <CardContent className={classes.parentCardContent}>Cargo</CardContent>
               </Card>
             </Grid>
           </Grid>
-        </Card>
+        </Paper>
       </Grid>
     </Grid>
   );
