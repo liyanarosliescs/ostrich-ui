1bbd9b7db3408f809b5acbf6b11435afc2f3b04c
liyanarosliescs <liyana.roslie@scs71.com>
2019-12-17 10:28:12 +0800

    cardform - add shipment formik

--- src/components/Shipper/CardForm.js
+++ src/components/Shipper/CardForm.js
@@ -5,8 +5,10 @@ import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
 import AddBox from "@material-ui/icons/AddBox";
 import DeleteBox from "@material-ui/icons/IndeterminateCheckBox";
+import { Formik, Field, Form, FieldArray } from "formik";
 import uuid from 'uuid';
 
 const useStyles = makeStyles(theme => ({
@@ -33,13 +35,56 @@ const useStyles = makeStyles(theme => ({
     display: "block",
     textAlign: "initial"
   },
-  media: {
-    height: 200
+  subtitle: {
+     textAlign: 'center'
   }
 }));
 
 export default function CardForm() {
   const classes = useStyles();
+
+  function generateId() {
+    return uuid.v4();
+  }
+
+  const initialId = generateId()
+
+  const initialValues = {
+    shipments: [
+      {
+        shipmentsId: initialId,
+        shipmentsType: "",
+        shipmentsOtherType: "",
+        transportsType: "",
+        noOfUnits: "",
+        ratePerUnit: "",
+        currency: "SGD",
+        isFrost: false,
+        isChiller: false,
+        isCa: false,
+        isDg: false,
+        isOpen: false,
+        isClose: false
+      }
+    ],
+    containers: [
+      {
+        containersId: initialId,
+        vehicleNo: "",
+        sealNo: "",
+        vgm: ""
+      }
+    ],
+    cargoes: [
+      {
+        cargoesId: initialId,
+        cargoName: "",
+        palletQuantity: "",
+        weight: "",
+        unNo: ""
+      }
+    ]
+  };
 
   const CargoCard = () => {
     return (
@@ -50,33 +95,84 @@ export default function CardForm() {
   }
 
   return (
-    <Grid container justify = "center">
-      <Grid item xs={12}>
-        <Paper className={classes.paper}>
-          Shipment
-          <Grid container justify = "center">
-            <Grid item xs={6}>
-              <Card className={classes.parentCard}>
-                <CardContent className={classes.parentCardContent}>Container</CardContent>
-                <Card className={classes.card}>
-                  <CardContent>
-                    <IconButton><AddBox/></IconButton>
-                    <div>
-                      Container A <br/>
-                      <IconButton><DeleteBox/></IconButton>
-                    </div>
-                  </CardContent>
-                </Card>
-              </Card>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          actions.setSubmitting(false);
+        }, 1000);
+      }}
+      >
+      {props => {
+        const {
+          values,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          dirty,
+          setFieldValue,
+          setFieldTouched,
+          isSubmitting
+        } = props;
+        return (
+          <Form>
+            <Grid container justify = "center">
+              <Grid item xs={12}>
+                <Paper className={classes.paper}>
+                  <Grid container justify = "center">
+                    <Grid item xs={12} className={classes.subtitle}>
+                      <FieldArray
+                        name="shipments"
+                        render={({ remove, push }) => (
+                          <div>
+                            <Typography variant="h6" className={classes.title}>
+                              Shipment
+                              <IconButton
+                                className= {classes.iconButton}
+                                onClick={() => push({ shipmentsType: "", transportsType: "", noOfUnits: "", ratePerUnit: "", currency: "SGD", isFrost: false, isChiller: false, isCa: false, isDg: false, isOpen: false, isClose: false   })}>
+                                <AddBox/>
+                              </IconButton>
+                            </Typography>
+                          </div>
+                        )}
+                      />
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Card className={classes.parentCard}>
+                        <CardContent className={classes.parentCardContent}>Container</CardContent>
+                        <Card className={classes.card}>
+                          <CardContent>
+                            <IconButton><AddBox/></IconButton>
+                            <div>
+                              Container A <br/>
+                              <IconButton><DeleteBox/></IconButton>
+                            </div>
+                          </CardContent>
+                        </Card>
+                      </Card>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Card className={classes.parentCard}>
+                        <CardContent className={classes.parentCardContent}>Cargo</CardContent>
+                          <Card className={classes.card}>
+                            <CardContent>
+                              <IconButton><AddBox/></IconButton>
+                              <div>
+                                Cargo A <br/>
+                                <IconButton><DeleteBox/></IconButton>
+                              </div>
+                            </CardContent>
+                          </Card>
+                      </Card>
+                    </Grid>
+                  </Grid>
+                </Paper>
+              </Grid>
             </Grid>
-            <Grid item xs={6}>
-              <Card className={classes.parentCard}>
-                <CardContent className={classes.parentCardContent}>Cargo</CardContent>
-              </Card>
-            </Grid>
-          </Grid>
-        </Paper>
-      </Grid>
-    </Grid>
+          </Form>
+        );
+      }}
+    </Formik>
   );
 }
