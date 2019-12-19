1261afbae1082461b3a142b65088daeb4721b677
liyanarosliescs <liyana.roslie@scs71.com>
2019-12-19 17:37:49 +0800

    NestedObjectsForm - use field instead of fieldarray and map shipments detail

--- src/components/Shipper/NestedObjectsForm.js
+++ src/components/Shipper/NestedObjectsForm.js
@@ -16,26 +16,45 @@ export default function NestedObjectsForm() {
   };
 
   return (
-    <div>
-      <h1>Social Profiles</h1>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={(values, actions) => {
-          // same shape as initial values
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            actions.setSubmitting(false);
-          }, 1000);
-          console.log(values);
-        }}>
-        <Form>
-          <Field name="shipments[0].shipmentsId" />
-          <Field name="shipments[0].shipmentsType" />
-          <Field name="shipments[1].shipmentsId" />
-          <Field name="shipments[1].shipmentsType" />
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
-    </div>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          actions.setSubmitting(false);
+        }, 1000);
+      }}
+      >
+      {props => {
+        const {
+          values,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          dirty,
+          setFieldValue,
+          setFieldTouched,
+          isSubmitting
+        } = props;
+        return (
+          <Form>
+            <div>
+              <h1>Shipment Profiles</h1>
+              {values.shipments.length > 0 &&
+                values.shipments.map((shipment, index) => (
+                  <div key={index}>
+                    <Field
+                      name={`shipments.${index}.shipmentsId`} />
+                    <Field
+                      name={`shipments.${index}.shipmentsType`} />
+                  </div>
+              ))}
+              <button type="submit">Submit</button>
+            </div>
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+          </Form>
+        );
+      }}
+    </Formik>
   );
 }
